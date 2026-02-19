75c8cb21390cac266c1ff389f50736701bd02a1c
LesegoKhaole <ST10455441@imconnect.edu.za>
2026-02-19 23:24:41 +0200

    Revert "Remove duplicate console output lines"
    This reverts commit d855791fbcfd78cbbe3eb083fe319b715a2f9deb.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#! One more modification");
-
-
+Console.WriteLine("Hello, C#! One more modification");
+Console.WriteLine("Hello, C#! One more modification");
 Console.WriteLine("Hello, C#! One more modification");
